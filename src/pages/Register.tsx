a17cd61122ba1cf78e3512619c9752aa82dc198c
Loay <loay_97@outlook.com>
2025-12-04 15:43:20 +0100

    Front-end - aanpassing domein azure

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import axios from "axios";
+import { API_BASE_URL } from '../services/api';
 
 const Register: React.FC = () => {
     const [email, setEmail] = useState("");
@@ -19,7 +20,7 @@ const Register: React.FC = () => {
         }
 
         try {
-            await axios.post("http://localhost:8080/auth/register", {
+            await axios.post(`${API_BASE_URL}/auth/register`, {
                 email,
                 password,
                 displayName
